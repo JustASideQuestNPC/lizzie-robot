55098f23eae9dd5754885780b6c3c52a50eacdd0
Joe Williams <joe.williams@oit.edu>
2024-02-28 10:36:29 -0800

    removed color-print version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import config_vars 
 from argparse import ArgumentParser # won't be used in the robot
+from robot import *
 
 parser = ArgumentParser()
 parser.add_argument('-q', '--quiet_logging', action='store_false')
@@ -9,13 +10,7 @@ args = parser.parse_args()
 
 config_vars.VERBOSE_LOGGING = args.quiet_logging
 
-# import the correct file based on whether we want color or not
-if args.no_color:
-    import robot_no_color as robot # change the namespace name so the same code still works
-else:
-    import robot
-
-robot = robot.Robot(
+robot = Robot(
     config_vars.ROBOT_START_POSITION,
     config_vars.ROBOT_START_HEADING,
     config_vars.ROBOT_START_COLOR
--- robot.py
+++ robot.py
@@ -1,107 +1,136 @@
+# version of the robot class without any color_print calls
 from vector import *
-from color_print import color_print
 from robot_paths import *
 from config_vars import *
 
+''' --- Main Robot Class --- '''
 class Robot:
-  def __init__(self, x, y):
-    self.position = Vector(x, y)
-    self.heading = 0
+    def __init__(self, position: tuple[float, float], heading: float, color: str):
+        self.position = Vector(position[0], position[1])
+        self.heading = heading
+        self.current_color = color # what color (or the center) we're currently at
 
-  def at_node(self, node: Vector) -> bool:
-    delta = node - self.position # Vector
-    return delta.mag() < NODE_SIZE
-  
-  def turn_to_angle(self, angle: int) -> None:
-    delta = angle - self.heading
-    # motor code here
-    self.heading = angle
-
-  def move_forward(self, motor_degrees: int) -> None:
-    # motor code here
-    pass # keeps python happy; remove this when you add motor code
-
-  def follow_path(self, path_name: str) -> None:
-    color_print(f"Moving along path '{path_name}'", foreground='magenta')
-    path = ROBOT_PATHS[path_name]
-    for i, node in enumerate(path):  
-      reached_node = False
-      while not reached_node:
-
-        if VERBOSE_LOGGING:
-          color_print(f'--- Moving to node {i} ---', foreground='blue')
-          print((
-            f'Node position: {node}\n'
-            f'Current position: {self.position}'
-          ))
-
-        if self.at_node(node):
-          if VERBOSE_LOGGING:
-            color_print('Already at node!\n', foreground='green')
-          else:
-            color_print(f'Already at node {i}', foreground='green')
-          reached_node = True
-        else:
-          delta = node - self.position
-
-          # the magnitude will be a float and the motor control functions only take ints,
-          # so we'll round to get as close as we can
-          mag_cm_raw = delta.mag()
-          mag_degrees_raw = mag_cm_raw * 23 # convert to degrees for maximum precision
-
-          mag_degrees_adjusted = round(mag_degrees_raw)
-          mag_cm_adjusted = mag_degrees_adjusted / 23
-          
-          mag_degrees_error = abs(mag_degrees_raw - mag_degrees_adjusted)
-          mag_cm_error = mag_degrees_error / 23
-
-          # adjust the heading
-          heading_raw = delta.heading()
-          heading_adjusted = round(heading_raw)
-          heading_error = abs(heading_raw - heading_adjusted)
-
-          if VERBOSE_LOGGING:
-            print((
-              f'Delta vector: {delta}\n\n'
-              f'Raw length: {round(mag_degrees_raw, 3)}° ({round(mag_cm_raw, 3)} cm)\n'
-              f'Adjusted length: {mag_degrees_adjusted}° ({round(mag_cm_adjusted, 3)} cm)\n'
-              f'Length error: {round(mag_degrees_error, 3)}° ({round(mag_cm_error, 3)} cm)\n\n'
-              f'Raw heading: {round(heading_raw, 3)}°\n'
-              f'Adjusted heading: {heading_adjusted}°\n'
-              f'Heading error: {round(heading_error, 3)}°\n'
-            ))
-
-          position_offset = vec_from_polar(mag_cm_adjusted, heading_adjusted)
-          if VERBOSE_LOGGING:
-            print(f'Adjusted vector: {position_offset}')
-
-          print(f'Moving to node {i}...', end='')
-          self.turn_to_angle(heading_adjusted)
-          self.move_forward(mag_degrees_adjusted)
-          color_print('done', foreground='green')
-
-          # update position
-          self.position += position_offset
-
-          if VERBOSE_LOGGING:
-            print((
-              f'Final position: {self.position}\n'
-              f'Final error: {round((node - self.position).mag(), 3)} cm'
-            ))
-
-          # we're probably at the node, but check again just to be safe
-          if self.at_node(node):
-            if VERBOSE_LOGGING:
-              color_print('Position on target, moving to next node\n', foreground='green')
-            reached_node = True
-          elif VERBOSE_LOGGING:
-              color_print('Position off target, retrying current node\n', foreground='red')
+    def at_node(self, node: Vector) -> bool:
+        delta = node - self.position # Vector
+        return delta.mag() < NODE_SIZE
     
-    # find the final error now that we've reached the end of the path
-    end_node = ROBOT_PATHS[path_name][-1]
-    color_print('--- Pathing complete! ---', foreground='magenta')
-    print((
-      f'Target position: {end_node}\n'
-      f'Actual position: {self.position}\n'
-      f'Error: {round((end_node - self.position).mag(), 3)} cm'
-    ))
+    def turn_to_angle(self, angle: int) -> None:
+        delta = angle - self.heading
+        # motor code here
+        self.heading = angle
+
+    def move_forward(self, motor_degrees: int) -> None:
+        # motor code here
+        pass # keeps python happy; remove this when you add motor code
+
+    # follows a node path; returns True if the end color was reached successfully
+    def move_to_color(self, target_color=str) -> int:
+        path_name = self.current_color + '->' + target_color
+        # if a direct path doesn't exist, try to work around it
+        if path_name not in ROBOT_PATHS:
+            # if the target is the center or we're at the center, give up and crash
+            if self.current_color == 'center' or target_color == 'center':
+                print("FATAL ERROR: Target color '{tc}' is unreachable from this position ({cc})!"
+                      .format(tc = target_color, cc=self.current_color))
+                return False
+            # otherwise, try to reach it via the center
+            else:
+                print((
+                    "No direct path exists from current position ({cc}) to target position '{tc}'; "
+                    'attempting to reach it via the center.')
+                    .format(cc = self.current_color, tc = target_color)
+                )
+                # catch the status codes from the two move calls and only return them if they failed
+                if not self.move_to_color('center'):
+                    return False
+                if not self.move_to_color(target_color):
+                    return False
+                return True # now return a sucess code
+
+        # normally we'd use f strings, but SPIKE python uses an older version of python that doesn't
+        # support them 
+        print("Moving along path '{pn}'".format(pn = path_name))
+        path = ROBOT_PATHS[path_name]
+
+        for i, node in enumerate(path):
+            if VERBOSE_LOGGING:
+                print((
+                    '--- Moving to node {i} ---\n'
+                    'Node position: {n}\n'
+                    'Current position: {p}')
+                    .format(i = i, n = node, p = self.position)
+                )
+
+            if self.at_node(node):
+                if VERBOSE_LOGGING:
+                    print('Already at node!\n')
+                else:
+                    print('Already at node {i}'.format(i = i))
+                continue # end this iteration of the loop and go straight to the next node
+
+            delta = node - self.position
+
+            # the magnitude will be a float and the motor control functions only take ints, so
+            # we'll round to get as close as we can
+            mag_cm_raw = delta.mag()
+            mag_degrees_raw = mag_cm_raw * 23 # convert to degrees for maximum precision
+
+            mag_degrees_adjusted = round(mag_degrees_raw)
+
+            # adjust the heading
+            heading_raw = delta.heading()
+            heading_adjusted = round(heading_raw)
+
+            if VERBOSE_LOGGING:
+                mag_cm_adjusted = mag_degrees_adjusted / 23
+                mag_degrees_error = abs(mag_degrees_raw - mag_degrees_adjusted)
+                mag_cm_error = mag_degrees_error / 23
+                heading_error = abs(heading_raw - heading_adjusted)
+                position_offset = vec_from_polar(mag_cm_adjusted, heading_adjusted)
+                print((
+                    'Delta vector: {d}\n\n'
+                    'Raw length: {v0}° ({v1} cm)\n'
+                    'Adjusted length: {mda}° ({v2} cm)\n'
+                    'Length error: {v3}° ({v4} cm)\n'
+                    'Raw heading: {v5}°\n'
+                    'Adjusted heading: {ha}°\n'
+                    'Heading error: {v6}°\n'
+                    'Adjusted vector: {po}\n')
+                    .format(d = delta, v0 = round(mag_degrees_raw, 3), v1 = round(mag_cm_raw, 3),
+                            mda = mag_degrees_adjusted, v2 = round(mag_cm_adjusted, 3),
+                            v3 = round(mag_degrees_error, 3), v4 = round(mag_cm_error, 3),
+                            v5 = round(heading_raw, 3), ha = heading_adjusted,
+                            v6 = round(heading_error, 3), po=position_offset)
+                )
+
+            print('Moving to node {i}...'.format(i = i), end = '')
+            self.turn_to_angle(heading_adjusted)
+            self.move_forward(mag_degrees_adjusted)
+            print('done')
+
+            # update position
+            self.position += position_offset
+
+            if VERBOSE_LOGGING:
+                print((
+                    'Expected position: {t}\n'
+                    'Final position: {p}\n'
+                    'Final error: {e} cm\n')
+                    .format(t = node, p = self.position, e = round((node - self.position).mag(), 3))
+                )
+
+        # find the final error now that we've reached the end of the path
+        if VERBOSE_LOGGING:
+            end_node = ROBOT_PATHS[path_name][-1]
+            print('--- Pathing complete! ---')
+            print((
+                'Target position: {en}\n'
+                'Actual position: {p}\n'
+                'Error: {v0} cm')
+                .format(en = end_node, p = self.position,
+                        v0 = round((end_node - self.position).mag(), 3))
+            )
+        
+        # update our color and return True to indicate a success
+        self.current_color = target_color
+        return True
